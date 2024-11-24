bfd7eb934fd6798ccd6ae2c032852a39309a77b0
Alexandre Noguez <alexandrenoguez_rs@yahoo.com.br>
2024-11-24 12:49:10 -0300

    fix: removed unused file

--- server/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import routeRouter from "./tripRoutes";
-
-const apiRouter = Router();
-
-// Registrar o roteador de "route"
-apiRouter.use("/route", routeRouter);
-
-export default apiRouter;
